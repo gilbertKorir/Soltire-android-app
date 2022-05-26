38a3b6f27d9ba4e2ab0dae8f7a5dca39d9b063af
gilbertKorir <jerikorir@gmail.com>
2022-05-26 13:24:29 +0300

    initial commit

---
+++ app/src/main/java/com/example/soltire/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.soltire;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.easyPlayButton) Button mEasyPlayButton;
+    private static final String[] consonants = new String[] {"B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"};
+    private static final String[] vowels = new String[] {"A", "E", "I", "O", "U", "Y"};
+
+    //capture the random needed 6 constants
+    private String consonant1 = consonants[ (int)Math.floor(Math.random() * 20)];
+    private String consonant2 = consonants[ (int)Math.floor(Math.random() * 20)];
+    private String consonant3 = consonants[ (int)Math.floor(Math.random() * 20)];
+    private String consonant4 = consonants[ (int)Math.floor(Math.random() * 20)];
+    private String consonant5 = consonants[ (int)Math.floor(Math.random() * 20)];
+    private String consonant6 = consonants[ (int)Math.floor(Math.random() * 20)];
+
+    //capture the vowels
+    private String vowel1 = vowels[ (int)Math.floor(Math.random() * 6)];
+    private String vowel2 = vowels[ (int)Math.floor(Math.random() * 6)];
+    private String vowel3 = vowels[ (int)Math.floor(Math.random() * 6)];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mEasyPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, play.class);
+                intent.putExtra("consonant1", consonant1);
+                intent.putExtra("consonant2", consonant2);
+                intent.putExtra("consonant3", consonant3);
+                intent.putExtra("consonant4", consonant4);
+                intent.putExtra("consonant5", consonant5);
+                intent.putExtra("consonant6", consonant6);
+                intent.putExtra("vowel1", vowel1);
+                intent.putExtra("vowel2", vowel2);
+                intent.putExtra("vowel3", vowel3);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/soltire/Score.java
@@ -0,0 +1,14 @@
+package com.example.soltire;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Score extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score);
+    }
+}
